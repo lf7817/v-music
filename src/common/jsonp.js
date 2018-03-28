eeda70a46aceece8e456963d048453e3cf04379f
lf7817 <535536456@qq.com>
2018-03-28 13:54:15 +0800

    备份

---
+++ src/common/jsonp.js
@@ -0,0 +1,28 @@
+import originJSONP from 'jsonp'
+
+const data2param = (data) => {
+  let url = ''
+  
+  Object.keys(data).forEach(key => {
+    let value = data[key] || ''
+    url += `&${key}=${encodeURIComponent(value)}` 
+  })
+
+  return url ? url.substring(1) : ''
+}
+
+const jsonp = (url, data, option) => {
+  url += (url.indexOf('?') < 0) ? '?' : '&' + data2param(data)
+  
+  return new Promise((resolve, reject) => {
+    originJSONP(url, option, (err, data) => {
+      if (!err) {
+        resolve(data)
+      } else {
+        reject(err)
+      }
+    })
+  })
+}
+
+export default jsonp
--- src/components/recommend/index.vue
+++ src/components/recommend/index.vue
@@ -1,3 +1,31 @@
 <template>
-  <div>recommend</div>
+  <div class="recommend">recommend</div>
 </template>
+
+<script>
+import { getRecommend } from '@/api/recommend'
+import { ERR_OK } from '@/constant'
+
+export default {
+  data () {
+    return {
+      recommends: []
+    }
+  },
+  created () {
+    this._getRecommend()
+  },
+  methods: {
+    async _getRecommend () {
+      try {
+        const res = await getRecommend()
+        if (res.code === ERR_OK) {
+          this.recommends = res.data.slider
+        }
+      } catch (e) {
+        throw new Error(e)
+      }
+    }
+  }
+}
+</script>
